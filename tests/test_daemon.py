6a4ef4dcdd71661ebc18caa1d74dfa434ab96019
Alejandro Piad <apiad@apiad.net>
2026-03-03 10:42:01 -0500

    test: add initial daemon state test

---
+++ tests/test_daemon.py
@@ -0,0 +1,13 @@
+"""
+Tests for the Harpo daemon.
+"""
+
+from harpo.daemon import DaemonState, HarpoDaemon
+
+
+def test_daemon_initial_state() -> None:
+    """
+    Checks if the HarpoDaemon initializes in IDLE state.
+    """
+    daemon = HarpoDaemon()
+    assert daemon.state == DaemonState.IDLE
